93559bb11117c9a42bb8e44992af176c7df7dddf
Vigneshbackend <vignesh.krishnasamy@codingmart.com>
2020-08-03 07:59:48 +0530

    added fingerprint authentication

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import MapView from 'react-native-maps';
+import {
+  View,
+  Text,
+  Button,
+  DeviceEventEmitter,
+  NativeModules,
+} from 'react-native';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
+// import MapView from 'react-native-maps';
+import {useEffect} from 'react';
 export default function LoginScreen({navigation}) {
   const NavigateButtonFunction = () => {
     navigation.navigate('LoaderScreen');
@@ -8,9 +16,27 @@ export default function LoginScreen({navigation}) {
   const NavigateButtonFunctionShopping = () => {
     navigation.navigate('ImageScreen');
   };
+
+  useEffect(() => {
+    DeviceEventEmitter.addListener(
+      'FINGERPRINT_SCANNER_AUTHENTICATION',
+      (msg) => {
+        console.log('false authentication');
+      },
+    );
+    Scan().then((success) =>
+      success ? console.log('sucesss') : console.log('failed'),
+    );
+  }, []);
+
+  async function Scan() {
+    return FingerprintScanner.authenticate({
+      description: 'Scan your fingerprint on the device scanner to continue',
+    });
+  }
   return (
     <>
-      <MapView />
+      {/* <MapView /> */}
       <View>
         <Text>Login Screen</Text>
         <Button onPress={NavigateButtonFunction} title="press me" />
